03b8a9d61fe44ec92b4a0794adf646e932e154d6
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-05-24 19:34:41 -0300

    classe produtoController finalizado

---
+++ src/main/java/io/github/jeffmartins/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,16 @@
+package io.github.jeffmartins.produtosapi.controller;
+
+import io.github.jeffmartins.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    @PostMapping
+    public void salvar(Produto produto){
+        System.out.println("Produto recebido" + produto);
+    }
+}
